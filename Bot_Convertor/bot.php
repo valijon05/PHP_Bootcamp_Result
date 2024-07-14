57323ab5cf8d13665c2d8b99807d74a42bd22229
valijon <valijoneshmamatov98@gmail.com>
2024-07-14 20:38:47 +0500

    HW

--- Bot_Convertor/bot.php
+++ Bot_Convertor/bot.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 $currency = new Currency();
 $bot      = new BotHandler();
-
+//Valijon
 if (isset($update->message)) {
     $message          = $update->message;
     $chat_id          = $message->chat->id;
