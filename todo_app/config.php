8fd5db61a0347bc3913d5c541066e179b6801cb3
valijon <valijoneshmamatov98@gmail.com>
2024-07-29 00:45:41 +0500

    One

---
+++ todo_app/config.php
@@ -0,0 +1,6 @@
+<?php
+
+declare(strict_types=1);
+
+$token = "7422856223:AAFzdJHamkQgtJg2lP6MOnsMrEWgE84Onb0";
+
--- todo_app/routes/telegram.php
+++ todo_app/routes/telegram.php
@@ -1,3 +1,41 @@
 <?php
 
 declare(strict_types=1);
+
+require 'config.php';
+
+$bot = new Bot($token);
+
+$router = new Router();
+
+if (isset($router->getUpdate()->message)) {
+    $message = $router->getUpdate()->message;
+    $chatId  = $message->chat->id;
+    $text    = $message->text;
+
+    if ($text === "/start") {
+        $bot->handleStartCommand($chatId);
+        return;
+    }
+
+    if ($text === "/add") {
+        $bot->handleAddCommand($chatId);
+        return;
+    }
+
+    if ($text === "/all") {
+        $bot->getAllTasks($chatId);
+        return;
+    }
+
+    $bot->addTask($chatId, $text);
+}
+
+if (isset($router->getUpdate()->callback_query)) {
+    $callbackQuery = $router->getUpdate()->callback_query;
+    $callbackData  = (int) $callbackQuery->data;
+    $chatId        = $callbackQuery->message->chat->id;
+    $messageId     = $callbackQuery->message->message_id;
+
+    $bot->handleInlineButton($chatId, $callbackData);
+}
